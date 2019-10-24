f631d8fdfb477cd9f89e43a58a7bfb8e61add6b1
rommelKayumba <40698204+rommelKayumba@users.noreply.github.com>
2019-10-24 12:28:44 +0200

    HomePage cs
    we define different level of access for each user.
    Get access to different functionalities

--- IMS(Inventory Management System)/InventoryManagement/Home.aspx.cs
+++ IMS(Inventory Management System)/InventoryManagement/Home.aspx.cs
@@ -7,18 +7,91 @@ using System.Web.UI.WebControls;
 
 public partial class Home : System.Web.UI.Page
 {
-    protected void Page_Load(object sender, EventArgs e)
+     protected void Page_Load(object sender, EventArgs e)
     {
+        Control LogoutLink = this.Master.FindControl("LogoutLink");
+        LogoutLink.Visible = true;
+
+    
+        LogoutLink.Visible = true;
+
+
         if (Session["user"] == null)
         {
             Response.Redirect("Login.aspx");
+        }else
+        {
+            Control userDislay = this.Master.FindControl("usernameDisplay");
+            var user = (Label) Master.FindControl("usernameDisplay");
+            user.Text = Session["user"].ToString();
+
         }
+
+        if (Session["Role"] != null)
+        {
+            if (Convert.ToInt32(Session["Role"]) == 1)
+            {
+                lblWelcome.InnerText = "You are Logged in as an Admin";
+                btnEntries.Visible = false;
+                btncliens.Visible = false;
+                btnSales.Visible = false;
+                btnSupplier.Visible = false;
+                btnPurchase.Visible = false;
+            }
+
+            if (Convert.ToInt32(Session["Role"]) == 4)
+            {
+                lblWelcome.InnerText = "You are Logged in as a Client";
+
+                btnEntries.Visible = false;
+                btncliens.Visible = false;
+                btnSales.Visible = false;
+                btnSupplier.Visible = false;
+                btnProduct.Visible = false;
+                btnStocks.Visible = false;
+              
+                
+            }
+
+            if (Convert.ToInt32(Session["Role"]) == 3)
+            {
+                lblWelcome.InnerText = "You are Logged in as a Clerck";
+                btnEntries.Visible = false;
+                btnSupplier.Visible = false;
+                btnProduct.Visible = false;
+                btnSales.Visible = false;
+                btnMatching.Visible = false;
+                btncliens.Visible = false;
+                
+            }
+
+
+            if (Convert.ToInt32(Session["Role"]) == 2)
+            {
+                lblWelcome.InnerText = "You are Logged in as a Manager";
+               
+                btncliens.Visible = false;
+                btnSales.Visible = false;
+                btnSupplier.Visible = false;
+                btnProduct.Visible = false;
+                btnMatching.Visible = false;
+                btnPurchase.Visible = false;
+
+            }
+
+
+
+        }
+
+
+
     }
 
     protected void Button4_Click(object sender, EventArgs e)
     {
         Response.Redirect("Product.aspx");
     }
+
 
     protected void Button3_Click(object sender, EventArgs e)
     {
@@ -29,6 +102,8 @@ public partial class Home : System.Web.UI.Page
     {
         Response.Redirect("Sale.aspx");
     }
+
+
 
     protected void Button1_Click(object sender, EventArgs e)
     {
