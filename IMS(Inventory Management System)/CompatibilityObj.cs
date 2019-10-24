a0f7ee41c0cf540de6c73275ea15054c3d8adbd0
rommelKayumba <40698204+rommelKayumba@users.noreply.github.com>
2019-10-24 11:55:20 +0200

    Compatibility class object
    we create this class to get the object, to save the compatibility rules to the matching rule table.

---
+++ IMS(Inventory Management System)/CompatibilityObj.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+/// <summary>
+/// Summary description for Compatibility
+/// </summary>
+public class CompatibilityObj
+{
+    public int ComponentAID;
+    public int ComponentBID;
+
+    public CompatibilityObj()
+    {
+        //
+        // TODO: Add constructor logic here
+        //
+    }
+}
