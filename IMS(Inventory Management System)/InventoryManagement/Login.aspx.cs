5cf5bdc510a908468986018f141e87ab6eb66a2d
rommelKayumba <40698204+rommelKayumba@users.noreply.github.com>
2019-10-28 20:56:21 +0200

    Level of access for each type of user
    we have  specified a role for each user to differentiate between Admin , Manager, Clerck and Customer. 
    Each one of these users has been given a unique code specific to their level of access

--- IMS(Inventory Management System)/InventoryManagement/Login.aspx.cs
+++ IMS(Inventory Management System)/InventoryManagement/Login.aspx.cs
@@ -21,6 +21,9 @@ public partial class Login : System.Web.UI.Page
 
     protected void btnLogin_Click(object sender, EventArgs e)
     {
+    int Role = 0;
+        bool success = false;
+    
         //try catch to check if the conncetion to the database if closed 
         try
         {
@@ -31,23 +34,68 @@ public partial class Login : System.Web.UI.Page
             Console.WriteLine(ex.Message);
         }
 
-        sqlconnection.Open();
-        string checkquery = "Select count(1) from Login where Username='"+txtUserName.Text+"' and Password='"+txtPassword.Text.Trim()+"'";
-        SqlCommand cmd = new SqlCommand(checkquery,sqlconnection);
-        int count = Convert.ToInt32(cmd.ExecuteScalar());
-        if (count == 1)
+        try
         {
-           //creates a session
-            Session["user"] = txtUserName.Text.Trim();
-            Response.Redirect("Home.aspx");
+            sqlconnection.Open();
+            string checkquery = "Select * from Login where Username='" + txtUserName.Text + "' and Password='" + txtPassword.Text.Trim() + "'";
+            SqlCommand cmd = new SqlCommand(checkquery, sqlconnection);
+            using (SqlDataReader mr = cmd.ExecuteReader())
+            {
+                if (mr.HasRows)
+                {
+                    while (mr.Read())
+                    {
+                        success = true;
+                        Role = (int) mr["Role"];
+               
+                
+                       
+                    }
+                }
+            }
+
+         
+            //int count = Convert.ToInt32(cmd.ExecuteScalar());
+            if (success)
+            {
+                //creates a session
+                Session["user"] = txtUserName.Text.Trim();
+                Session["Role"] = Role;
+
+                if (Role == 4)
+                {
+                    Session["level"] = "Customer";
+                }
+                if(Role == 3)
+                {
+                    Session["level"] = "Stock";
+                }
+                if (Role == 2)
+                {
+                    Session["level"] = "Manager";
+                }
+                if (Role == 1)
+                {
+                    Session["level"] = "Admin";
+
+                }
+
+                Response.Redirect("Home.aspx");
+
+            }
+            else
+            {
+                //lebel error if the login is not succeful. if user input incorrect username or password 
+                lblerror.Text = "Login Failed. Incorrect Username or Password!";
+            }
+            sqlconnection.Close();
+
+        }catch(Exception ex)
+        {
+            Console.WriteLine(ex.Message);
             
         }
-        else
-        {
-            //lebel error if the login is not succeful. if user input incorrect username or password 
-            lblerror.Text = "Login Failed. Incorrect Username or Password!";
-        }
-        sqlconnection.Close();
+
     }
 
     // Thi sbutton refreshs the the fields .of user name and password 
