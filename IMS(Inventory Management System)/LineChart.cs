78a6335ef936af0a93947c4bd82f0894ef4bf280
rommelKayumba <40698204+rommelKayumba@users.noreply.github.com>
2019-10-28 20:09:18 +0200

    LineCharts object class
    get the list of items and orders in order to draw a charts for the reports.
    we create objects to get to draw the charts

---
+++ IMS(Inventory Management System)/LineChart.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+/// <summary>
+/// Summary description for LineChart
+/// </summary>
+public class LineChart
+{
+
+    public List<SeriesItems> series;
+    public string[] fetcheData
+        ;
+    public LineChart(List<SeriesItems> iSeries, string[] iFecthes)
+    {
+        //
+        // TODO: Add constructor logic here
+        //
+        series = iSeries;
+        fetcheData = iFecthes;
+
+    }
+
+    public LineChart()
+    {
+        //
+        // TODO: Add constructor logic here
+        //
+    }
+}
