1c320f56b7cdd0662e88891b22c1b80b9d5c13a5
rommelKayumba <40698204+rommelKayumba@users.noreply.github.com>
2019-10-28 21:05:33 +0200

    Series class object
    in this class we get and set the name and data to populate on the series of the charts

---
+++ IMS(Inventory Management System)/SeriesItems.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+/// <summary>
+/// Summary description for SeriesItems
+/// </summary>
+public class SeriesItems
+{
+
+    public string name { get; set; }
+
+    public int[] data { get; set; }
+
+    public SeriesItems()
+    {
+
+        //
+        // TODO: Add constructor logic here
+        //
+    }
+}
